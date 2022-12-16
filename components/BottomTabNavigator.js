7d159d2544f95348a36444e9c7def62d304e96d0
itzTVX <94137584+itzTVX@users.noreply.github.com>
2022-12-16 17:21:42 -0300

    Update BottomTabNavigator.js

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -27,10 +27,11 @@ export default class BottomTabNavigator extends Component {
           <Screen name="Histórico de Locações" component={RideHistoryScreen} />
           */}
 
-          {/*
+          {
           <Tab.Screen name="Alugue uma Bicicleta" component={RideScreen} />
           <Tab.Screen name="Histórico de Locações" component={RideHistoryScreen} />
-          */}
+          }
+
         </Tab.Navigator>
       </NavigationContainer>
     );
